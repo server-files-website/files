bfe15dddd1f6e546e3ea82bf8695d4e19f051798
server-files-website <56220522+server-files-website@users.noreply.github.com>
2019-11-09 00:37:19 -0800

    Add files via upload

---
+++ copyPS.py
@@ -0,0 +1,9 @@
+#Website:https://www.yehia.online/
+import pyperclip
+import time
+yehia = "Welcome to yehia website"
+pyperclip.copy(yehia)
+print("Wait the paste")
+time.sleep( 5 )
+pyperclip.paste()
+   
