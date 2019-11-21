42d7cd5a5935ca076bec4397473d05e64a9f62c0
server-files-website <56220522+server-files-website@users.noreply.github.com>
2019-11-21 00:39:59 -0800

    Create m1.js

---
+++ m1.js
@@ -0,0 +1,24 @@
+//<![CDATA[
+
+    $(function(){
+      
+$('input.checkbutton').on('change', function() {
+    $('input.checkbutton').not(this).prop('checked', false);
+});
+
+$(".textBox").focus(function() {
+    $(".checkbutton").prop("checked", false);
+});
+
+$(document).ready(function() {
+    $(".checkbutton").change(function() {
+        if ($(this).not(":checked")) {
+            $('.textBox').val("");
+        }
+    });
+});
+
+
+    });
+
+  //]]>
